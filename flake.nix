719d9c6680a3a341effb8bc6be50acf0046f6cb3
monologique <git@monologique.me>
2025-04-03 16:23:56 +0200

    Add ultra-scroll

--- flake.nix
+++ flake.nix
@@ -64,6 +64,18 @@
             setup
             spacious-padding
             treesit-grammars.with-all-grammars
+            (pkgs.emacsPackages.melpaBuild rec {
+              pname = "ultra-scroll";
+              version = "0.3.2"; # You can use the latest version or a specific Git commit hash
+
+              src = pkgs.fetchFromGitHub {
+                owner = "jdtsmith";
+                repo = "ultra-scroll";
+                rev = "master"; # Or a specific commit or version tag
+                sha256 = "MHlHsciVPNyvqwkop9arOQ1VTV5POxJZ+z+IZo/PrMM=";
+              };
+            })
+
             vertico
           ]
         );
