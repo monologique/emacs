b5333547da5117925e2f0bbae4d43d4e1106d2e3
monologique <git@monologique.me>
2025-04-06 11:13:12 +0200

    Bump ultra-scroll

--- nix/packages/ultra-scroll.nix
+++ nix/packages/ultra-scroll.nix
@@ -4,12 +4,12 @@
 }:
 melpaBuild {
   pname = "ultra-scroll";
-  version = "0.3.2"; # You can use the latest version or a specific Git commit hash
+  version = "0.3.4"; # You can use the latest version or a specific Git commit hash
 
   src = fetchFromGitHub {
     owner = "jdtsmith";
     repo = "ultra-scroll";
     rev = "master"; # Or a specific commit or version tag
-    sha256 = "MHlHsciVPNyvqwkop9arOQ1VTV5POxJZ+z+IZo/PrMM=";
+    sha256 = "aAYPIsyoSAM6Qy8hZQ4+KJAd9OBkQr8U7HEqWAWRhaI=";
   };
 }
