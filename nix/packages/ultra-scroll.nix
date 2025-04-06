4d7219040184a84133bd9e5aa97bac196beb4ca7
monologique <git@monologique.me>
2025-04-06 15:54:06 +0200

    Remove comments

--- nix/packages/ultra-scroll.nix
+++ nix/packages/ultra-scroll.nix
@@ -4,12 +4,12 @@
 }:
 melpaBuild {
   pname = "ultra-scroll";
-  version = "0.3.4"; # You can use the latest version or a specific Git commit hash
+  version = "0.3.4";
 
   src = fetchFromGitHub {
     owner = "jdtsmith";
     repo = "ultra-scroll";
-    rev = "master"; # Or a specific commit or version tag
+    rev = "master";
     sha256 = "aAYPIsyoSAM6Qy8hZQ4+KJAd9OBkQr8U7HEqWAWRhaI=";
   };
 }
